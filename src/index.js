91a4c602f7be1428a724e88eba78133165ebbadb
Alan Martins <alanhorcrux@gmail.com>
2021-03-11 16:42:44 -0300

    Obtendo carteira

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const customers = [];
 
-app.post("/", (request, response) => {
+app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
     const customersAlreadyExists = customers.some((customer) => customer.cpf === cpf);
@@ -27,5 +27,17 @@ app.post("/", (request, response) => {
 
     return response.status(201).send(customers);
 });
+
+app.get("/statement", (request, response) => {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(404).json({error: "Not found!"})
+    }
+    
+    return response.json(customer.statement);
+})
 
 app.listen(3333);
